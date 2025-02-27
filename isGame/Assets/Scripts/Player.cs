8ce910cb13b548754b09c6c75bb6d422c13e513e
Oghenemarho Akpenyi <marhoethan@gmail.com>
2025-02-27 09:02:38 -0600

    changes

--- isGame/Assets/Scripts/Player.cs
+++ isGame/Assets/Scripts/Player.cs
@@ -2,24 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float playerSpeed;
-
-    private Rigidbody rb;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    public float playerSpeed = 5f;
+    public float rotationSpeed = 700f;
 
     void Update()
     {
         Vector3 moveVec = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        transform.position += moveVec * playerSpeed * Time.DeltaTime;
-    }
 
-    private void FixedUpdate()
-    {
-        
     }
 }
