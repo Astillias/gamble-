e580e2c0eddfcc3cc667eb9d4ac8b092acc1eae8
Oghenemarho Akpenyi <marhoethan@gmail.com>
2025-03-04 08:38:14 -0600

    player movement and adjustments to scene design

--- isRealGame/Assets/Scripts/MoveCam.cs
+++ isRealGame/Assets/Scripts/MoveCam.cs
@@ -2,18 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCam : MonoBehaviour
+public class CharacterController3D : MonoBehaviour
 {
-    public Transform cameraPosition;
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public LayerMask groundLayer;
+    private Rigidbody rb;
+    private bool isGrounded;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position = cameraPosition.position;
+        Move();
+        Jump();
+        Interact();
+    }
+
+    void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        rb.MovePosition(rb.position + move);
+    }
+
+    void Jump()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, 0.1f, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void Interact()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 2f))
+            {
+                if (hit.transform.CompareTag("Interactable"))
+                {
+                    Debug.Log("Interacted with " + hit.transform.name);
+                }
+            }
+        }
     }
 }
