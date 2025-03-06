248d457ca29ea7563e078283b87c6b39db2a72b6
Oghenemarho Akpenyi <marhoethan@gmail.com>
2025-03-06 09:00:17 -0600

    coinflip interacion

---
+++ isRealGame/Assets/Scripts/Interactor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    [SerializeField] private Transform interactionPoint;
+    [SerializeField] private float interactionPointRadius;
+    [SerializeField] private LayerMask interactionMask;
+
+    private readonly Collider[] _colliders = new Collider[3];
+    [SerializeField] private int _numFound;
+
+    private void Update()
+    {
+        _numFound = Physics.OverlapSphereNonAlloc(interactionPoint.position, interactionPointRadius, _colliders, interactionMask);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionPoint.position, interactionPointRadius);
+    }
+}
