8a218a0b52a57d24a271b36bac739dbaec541cd4
Oghenemarho Akpenyi <marhoethan@gmail.com>
2025-03-07 09:00:10 -0600

    moevment olayer failll

--- isRealGame/Assets/Scripts/MoveCam.cs
+++ isRealGame/Assets/Scripts/MoveCam.cs
@@ -1,58 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveCam : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 5f;
-    public LayerMask groundLayer;
-    private Rigidbody rb;
-    private bool isGrounded;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    #region "Variables"
+    public Rigidbody Rigid;
+    public float MouseSensitivity;
+    public float MoveSpeed;
+    public float JumpForce;
+    #endregion
 
     void Update()
     {
-        Move();
-        Jump();
-        Interact();
-    }
-
-    void Move()
-    {
-        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float moveZ = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-
-        Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        rb.MovePosition(rb.position + move);
-    }
-
-    void Jump()
-    {
-        isGrounded = Physics.CheckSphere(transform.position, 0.1f, groundLayer);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
-    }
-
-    void Interact()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 2f))
-            {
-                if (hit.transform.CompareTag("Interactable"))
-                {
-                    Debug.Log("Interacted with " + hit.transform.name);
-                }
-            }
-        }
+        Rigid.MoveRotation(Rigid.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0)));
+        Rigid.velocity.Set((Input.GetAxis("Vertical") * MoveSpeed), (Input.GetAxis("Horizontal") * MoveSpeed), 0);
+        if (Input.GetKeyDown("space"))
+            Rigid.AddForce(transform.up * JumpForce);
     }
 }
